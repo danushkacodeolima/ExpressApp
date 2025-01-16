757d18d979c6f5c069f1a53ce39c2a6c04b6169c
danushkacodeolima <danushka@codeolima.com>
2025-01-16 09:21:57 +0530

    feat(app.js):Added api routes

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var itemRoutes = require('./router/itemroutes');
 
 var app = express();
+app.use(express.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('api/v1/items', itemRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
